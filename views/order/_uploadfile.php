212efef199e958af1ac7aeb1a2f276ee2d3f7d85
Gits <gitau.ag@gmail.com>
2018-02-16 00:26:27 +0300

    essay

--- views/order/_uploadfile.php
+++ views/order/_uploadfile.php
@@ -2,7 +2,7 @@
 
 <tr class="cart_item">
     <td  style="padding-top: 5px; padding-bottom: 5px">
-        <span><a href="<?= Yii::$app->request->baseUrl?>/images/order/<?=$model->name ?>"  download><?= $model->name ?></a></span>
+        <span><a href="<?= Yii::$app->request->baseUrl?>/images/uploads/<?=$model->name ?>"  download><?= $model->name ?></a></span>
     </td>
     <td  style="padding-top: 5px; padding-bottom: 5px">
         <span><a data-method="post" type="button" class="btn btn-md btn-danger" href="<?= \yii\helpers\Url::to(['order/upload-delete','order'=>$model->order_number, 'file'=>$model->name])?>">Delete</a></span>
