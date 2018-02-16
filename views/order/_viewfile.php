7c8b53399b5b4c2dae88caa40e90f716a859941e
Gits <gitau.ag@gmail.com>
2018-02-16 15:23:11 +0300

    essay

--- views/order/_viewfile.php
+++ views/order/_viewfile.php
@@ -4,10 +4,11 @@
     <td  style="padding-top: 5px; padding-bottom: 5px">
         <?php
         $order_file =  $model->attached.'-'.$model->file_date.'.'.$model->file_extension;
+        Yii::setAlias('@ordersource', 'https://doctorateessays.com');
         ?>
         <span><?= $model->attached.'.'.$model->file_extension ?></span>
     </td>
     <td  style="padding-top: 5px; padding-bottom: 5px">
-        <span><a type="button" class="btn btn-md btn-danger" href="https://doctorateessays.com/images/order/<?=$order_file?>"><i class="fa fa-download fa-2x" aria-hidden="true"></i></a></span>
+        <span><a type="button" class="btn btn-md btn-danger" href="<?= \yii\helpers\Url::to('@ordersource').'/images/order/'.$order_file.''?>"><i class="fa fa-download fa-2x" aria-hidden="true"></i></a></span>
     </td>
 </tr>
