08762052fe77cabb32f3ae2f7ac140d96867891f
Anthony Gitau <gitau.ag@gmail.com>
2020-10-10 19:06:17 +0300

    remove ignored files

---
+++ config/params.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@doctorateessays.com',
+    'managerEmail' => 'manager@doctorateessays.com',
+    'supportEssay'=>'Doctorate Essays Support',
+    'supportEmail' => 'support@doctorateessays.com',
+    'successUrl'=>"https://doctorateessays.com/wallet/paypal",
+    'cancelUrl'=>"https://doctorateessays.com/wallet/cancel?success=false",
+    'successUrl2'=>"https://doctorateessays.com/wallet/pay",
+    'cancelUrl2'=>"https://doctorateessays.com/wallet/order-cancel?success=false",
+    'clientId'=>'AYmJcHoiMofhmWwIcc88MOl52PFW6IUdnF10v6vuz1bXLoWm9MMTAKIbD7WZbvbEJ4nbBg3Onx0tc995',
+    'clientSecret'=>'EAXyxQGfZhGPzbRNdHJWbJPMNFmRf9rpeNXxyErrVPtWcA0jsQKk-otFu_zDybDhFSN9Uqt79lO9GbVd',
+    'mode'=> 'live',
+    'couponcode'=>'PHDESSAYS',
+    'couponamt'=> 0.9,
+    'code2'=>'ESSAY25',
+    'code2amt'=> 0.75,
+    'code3'=>'ESSAY50',
+    'code3amt'=> 0.5,
+    'user.passwordResetTokenExpire' => 3600,
+    'user.rememberMeDuration'=>3600,
+    '1'=> 'https://doctorateessays.com',
+    '2'=> 'https://topratedprofessors.com',
+    'recaptcha_api_key'=>'6LcXFfEUAAAAACyYLjoG52fn9q1EwdrXfdW7KQNc',
+    'recaptcha_api_secret'=>'6LcXFfEUAAAAAAJutrdOinyu-dvHxrdp3n25e6H1'
+];
