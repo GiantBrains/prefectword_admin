876b37b8ab38dc0a46aebc18362383df611f8a23
Anthony Gitau <gitau.ag@gmail.com>
2021-09-30 12:03:02 +0300

    implement admin users

--- views/auth-assignment/_form.php
+++ views/auth-assignment/_form.php
@@ -16,8 +16,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'user_id')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
